d1a925554d3d8fe4f302d28b43b26b22d6b045eb
Alee.Van2812 <alee.van2812@gmail.com>
2024-01-12 15:42:28 +0700

    Add project files.

---
+++ Alee_BulkyWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Alee_BulkyWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
