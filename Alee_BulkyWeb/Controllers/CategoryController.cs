8c096786cd487172a98bec9d69a57647c2baa809
Alee.Van2812 <alee.van2812@gmail.com>
2024-01-12 23:33:17 +0700

    008 Add Category Controller

---
+++ Alee_BulkyWeb/Controllers/CategoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Alee_BulkyWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
