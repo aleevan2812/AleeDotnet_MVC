270137c6c091e82d58ecc3924a31b6424891b29b
Alee.Van2812 <alee.van2812@gmail.com>
2024-01-15 21:56:25 +0700

    011 Get all Categories

--- Alee_BulkyWeb/Controllers/CategoryController.cs
+++ Alee_BulkyWeb/Controllers/CategoryController.cs
@@ -1,11 +1,21 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Alee_BulkyWeb.Data;
+using Alee_BulkyWeb.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Alee_BulkyWeb.Controllers
 {
     public class CategoryController : Controller
     {
+        private readonly ApplicationDbContext _db;
+
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public IActionResult Index()
         {
+            List<Category> objCategoryList = _db.Categories.ToList();
             return View();
         }
     }
