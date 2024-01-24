b3d04fd0a3b427b71cb4f9aa0e8ca6c385960152
Alee.Van2812 <alee.van2812@gmail.com>
2024-01-24 22:24:19 +0700

    024 Get Category Details to Edit

--- Alee_BulkyWeb/Controllers/CategoryController.cs
+++ Alee_BulkyWeb/Controllers/CategoryController.cs
@@ -39,5 +39,31 @@ namespace Alee_BulkyWeb.Controllers
             }
             return View();
         }
+
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id == 0)
+                return NotFound();
+            Category categoryFromDb = _db.Categories.Find(id);
+            if (categoryFromDb == null)
+                return NotFound();
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())
+            {
+                ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+            }
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
     }
 }
