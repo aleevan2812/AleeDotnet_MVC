cb14159919f84e15de60d1ae2179a74cf309b11f
Alee.Van2812 <alee.van2812@gmail.com>
2024-01-30 15:03:54 +0700

    03 - 004 Create first Razor Page

---
+++ AleeWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AleeWebRazor_Temp.Pages.Categories
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
