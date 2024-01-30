b5929161824d09114caadcf314ca4d9f20db1f88
Alee.Van2812 <alee.van2812@gmail.com>
2024-01-30 22:32:57 +0700

    03 - 008 Edit and Delete Category

---
+++ AleeWebRazor_Temp/Pages/Categories/Delete.cshtml.cs
@@ -0,0 +1,37 @@
+using AleeWebRazor_Temp.Data;
+using AleeWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AleeWebRazor_Temp.Pages.Categories
+{
+    [BindProperties]
+    public class DeleteModel : PageModel
+    {
+        private readonly ApplicationDbContext _db;
+
+        //[BindProperty]
+        public Category Category { get; set; }
+
+        public DeleteModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
+        public void OnGet(int? id)
+        {
+            if (id != null && id != 0)
+                Category = _db.Categories.Find(id);
+        }
+
+        public IActionResult OnPost()
+        {
+            Category? obj = _db.Categories.Find(Category.Id);
+            if (obj == null)
+                return NotFound();
+            _db.Remove(obj);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
+        }
+    }
+}
