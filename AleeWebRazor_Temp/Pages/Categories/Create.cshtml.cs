17a7e9ac0d06269cdd18df82bc3c45a3ed6245aa
Alee.Van2812 <alee.van2812@gmail.com>
2024-01-30 15:57:48 +0700

    03 - 007 Create Category Post Handler

--- AleeWebRazor_Temp/Pages/Categories/Create.cshtml.cs
+++ AleeWebRazor_Temp/Pages/Categories/Create.cshtml.cs
@@ -1,12 +1,16 @@
 using AleeWebRazor_Temp.Data;
 using AleeWebRazor_Temp.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AleeWebRazor_Temp.Pages.Categories
 {
+    [BindProperties]
     public class CreateModel : PageModel
     {
         private readonly ApplicationDbContext _db;
+
+        //[BindProperty]
         public Category Category { get; set; }
 
         public CreateModel(ApplicationDbContext db)
@@ -16,6 +20,13 @@ namespace AleeWebRazor_Temp.Pages.Categories
 
         public void OnGet()
         {
+        }
+
+        public IActionResult OnPost()
+        {
+            _db.Categories.Add(Category);
+            _db.SaveChanges();
+            return RedirectToPage("Index");
         }
     }
 }
