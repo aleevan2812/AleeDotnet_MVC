89c23fda58bc5e76ebb86817028b6c5a50076568
Alee.Van2812 <alee.van2812@gmail.com>
2024-01-30 15:33:45 +0700

    03 - 005 Display all Categories

--- AleeWebRazor_Temp/Pages/Categories/Index.cshtml.cs
+++ AleeWebRazor_Temp/Pages/Categories/Index.cshtml.cs
@@ -1,12 +1,22 @@
-using Microsoft.AspNetCore.Mvc;
+using AleeWebRazor_Temp.Data;
+using AleeWebRazor_Temp.Models;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace AleeWebRazor_Temp.Pages.Categories
 {
     public class IndexModel : PageModel
     {
+        private readonly ApplicationDbContext _db;
+        public List<Category> CategoryList { get; set; }
+
+        public IndexModel(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+
         public void OnGet()
         {
+            CategoryList = _db.Categories.ToList();
         }
     }
 }
