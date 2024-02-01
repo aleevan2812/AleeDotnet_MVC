88cc0403102f13e4d48f04a08164c3dba1bcf3d1
Alee.Van2812 <alee.van2812@gmail.com>
2024-02-01 23:48:24 +0700

    05 - 003 Implement ICategoryRepository

---
+++ AleeVan.DataAccess/Respository/IRespository/ICategoryRepository.cs
@@ -0,0 +1,11 @@
+﻿using AleeVan.Models;
+
+namespace AleeVan.DataAccess.Respository.IRespository
+{
+    public interface ICategoryRepository : IRespository<Category>
+    {
+        void Update(Category obj);
+
+        void Save();
+    }
+}
