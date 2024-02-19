fcdcf4b7134b14123e4b634eab9960f0c1140d31
Alee.Van2812 <alee.van2812@gmail.com>
2024-02-19 15:35:10 +0700

    06 - 006 Add Foreign Key in EF Core

--- AleeVan.DataAccess/Migrations/20240219080035_addForeignKeyForCategoryProductRelation.Designer.cs
+++ AleeVan.DataAccess/Migrations/20240219080035_addForeignKeyForCategoryProductRelation.Designer.cs
@@ -3,6 +3,7 @@ using AleeBook.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AleeBook.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240219080035_addForeignKeyForCategoryProductRelation")]
+    partial class addForeignKeyForCategoryProductRelation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
