2c2794a16b016f0a5349cc66f20b456f0abbddf7
Alee.Van2812 <alee.van2812@gmail.com>
2024-02-19 15:40:08 +0700

    06 - 007 Add Image Url Column

--- AleeVan.DataAccess/Migrations/20240219083900_addImageUrlToProduct.Designer.cs
+++ AleeVan.DataAccess/Migrations/20240219083900_addImageUrlToProduct.Designer.cs
@@ -3,6 +3,7 @@ using AleeBook.DataAccess.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace AleeBook.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240219083900_addImageUrlToProduct")]
+    partial class addImageUrlToProduct
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
--- AleeVan.Models/Product.cs
+++ AleeVan.Models/Product.cs
@@ -42,5 +42,7 @@ namespace AleeBook.Models
 
         [ForeignKey("CategoryId")]
         public Category Category { get; set; }
+
+        public string ImageUrl { get; set; }
     }
 }
