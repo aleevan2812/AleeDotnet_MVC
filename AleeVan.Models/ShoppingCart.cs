f1647d43eab9623311b7d0311f36bfa05279626b
Alee.Van2812 <alee.van2812@gmail.com>
2024-03-21 14:49:07 +0700

    10 - 001 Add Shopping Cart Model

---
+++ AleeVan.Models/ShoppingCart.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace AleeBook.Models
+{
+    public class ShoppingCart
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+
+        [ForeignKey("ProductId")]
+        [ValidateNever]
+        public Product Product { get; set; }
+
+        public int Count { get; set; }
+        public string ApplicationUserId { get; set; }
+
+        [ForeignKey("ApplicationUserId")]
+        [ValidateNever]
+        public ApplicationUser ApplicationUser { get; set; }
+    }
+}
