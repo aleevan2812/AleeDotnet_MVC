12993a2d205db698b043e5a46cea1486bceefa1d
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-04-12 15:44:58 +0700

    12 - 012 Ship Order

--- Alee_BulkyWeb/Areas/Admin/Controllers/OrderController.cs
+++ Alee_BulkyWeb/Areas/Admin/Controllers/OrderController.cs
@@ -9,6 +9,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace AleeBookWeb.Areas.Admin.Controllers;
 
 [Area("admin")]
+[Authorize]
 public class OrderController : Controller
 {
     private readonly IUnitOfWork _unitOfWork;
@@ -48,18 +49,49 @@ public class OrderController : Controller
         orderHeaderFromDb.State = OrderVM.OrderHeader.State;
         orderHeaderFromDb.PostalCode = OrderVM.OrderHeader.PostalCode;
 
-        if (!string.IsNullOrEmpty((OrderVM.OrderHeader.Carrier)))
+        if (!string.IsNullOrEmpty(OrderVM.OrderHeader.Carrier))
             orderHeaderFromDb.Carrier = OrderVM.OrderHeader.Carrier;
-        if (!string.IsNullOrEmpty((OrderVM.OrderHeader.TrackingNumber)))
+        if (!string.IsNullOrEmpty(OrderVM.OrderHeader.TrackingNumber))
             orderHeaderFromDb.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
-        
+
         _unitOfWork.OrderHeader.Update(orderHeaderFromDb);
         _unitOfWork.Save();
 
         TempData["Success"] = "Order Details Updated Successfully";
-        
-        return RedirectToAction(nameof(Details), new {orderId = orderHeaderFromDb.Id});
+
+        return RedirectToAction(nameof(Details), new { orderId = orderHeaderFromDb.Id });
     }
+
+    [HttpPost]
+    [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+    public IActionResult StartProcessing()
+    {
+        _unitOfWork.OrderHeader.UpdateStatus(OrderVM.OrderHeader.Id, SD.StatusInProcess);
+        _unitOfWork.Save();
+        TempData["Success"] = "Order Details Updated Successfully";
+
+        return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.Id });
+    }
+    
+    [HttpPost]
+    [Authorize(Roles = SD.Role_Admin + "," + SD.Role_Employee)]
+    public IActionResult ShipOrder()
+    {
+        var orderHeader = _unitOfWork.OrderHeader.Get(u => u.Id == OrderVM.OrderHeader.Id);
+        orderHeader.TrackingNumber = OrderVM.OrderHeader.TrackingNumber;
+        orderHeader.Carrier = OrderVM.OrderHeader.Carrier;
+        orderHeader.OrderStatus = SD.StatusShipped;
+        orderHeader.ShippingDate = DateTime.Now;
+        if(orderHeader.PaymentStatus == SD.PaymentStatusDelayedPayment)
+            orderHeader.PaymentDueDate = (DateTime.Now.AddDays(30));
+        _unitOfWork.OrderHeader.Update(orderHeader);
+        _unitOfWork.Save();
+        
+        TempData["Success"] = "Order Shipped Successfully";
+    
+        return RedirectToAction(nameof(Details), new { orderId = OrderVM.OrderHeader.Id });
+    }
+
 
     #region API CALLS
 
@@ -67,9 +99,11 @@ public class OrderController : Controller
     public IActionResult GetAll(string status)
     {
         IEnumerable<OrderHeader> objOrderHeaders;
-        
-        if(User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+
+        if (User.IsInRole(SD.Role_Admin) || User.IsInRole(SD.Role_Employee))
+        {
             objOrderHeaders = _unitOfWork.OrderHeader.GetAll(includeProperties: "ApplicationUser").ToList();
+        }
         else
         {
             var claimsIdentity = (ClaimsIdentity)User.Identity;
@@ -77,7 +111,7 @@ public class OrderController : Controller
             // noi luu tru userId
 
             objOrderHeaders = _unitOfWork.OrderHeader.GetAll(u => u.ApplicationUserId == userId,
-                includeProperties: "ApplicationUser");
+                "ApplicationUser");
         }
 
         switch (status)
