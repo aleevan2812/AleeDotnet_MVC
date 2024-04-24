002051e98597cf7ec4ee5718097e72d7a76e26c7
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-04-24 22:02:37 +0700

    13 - 005 Create View Component

--- Alee_BulkyWeb/Areas/Identity/Pages/Account/Logout.cshtml.cs
+++ Alee_BulkyWeb/Areas/Identity/Pages/Account/Logout.cshtml.cs
@@ -1,45 +1,36 @@
 ﻿// Licensed to the .NET Foundation under one or more agreements.
 // The .NET Foundation licenses this file to you under the MIT license.
+
 #nullable disable
 
-using System;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
-using Microsoft.Extensions.Logging;
 
-namespace AleeBookWeb.Areas.Identity.Pages.Account
+namespace AleeBookWeb.Areas.Identity.Pages.Account;
+
+public class LogoutModel : PageModel
 {
-    public class LogoutModel : PageModel
+    private readonly ILogger<LogoutModel> _logger;
+    private readonly SignInManager<IdentityUser> _signInManager;
+
+    public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
     {
-        private readonly SignInManager<IdentityUser> _signInManager;
-        private readonly ILogger<LogoutModel> _logger;
+        _signInManager = signInManager;
+        _logger = logger;
+    }
 
-        public LogoutModel(SignInManager<IdentityUser> signInManager, ILogger<LogoutModel> logger)
-        {
-            _signInManager = signInManager;
-            _logger = logger;
-        }
+    public async Task<IActionResult> OnPost(string returnUrl = null)
+    {
+        await _signInManager.SignOutAsync();
 
-        public async Task<IActionResult> OnPost(string returnUrl = null)
-        {
-            await _signInManager.SignOutAsync();
-            
-            HttpContext.Session.Clear(); // remove session when logout
-            
-            _logger.LogInformation("User logged out.");
-            if (returnUrl != null)
-            {
-                return LocalRedirect(returnUrl);
-            }
-            else
-            {
-                // This needs to be a redirect so that the browser performs a new
-                // request and the identity for the user gets updated.
-                return RedirectToPage();
-            }
-        }
+        // HttpContext.Session.Clear(); // remove session when logout
+        // we dont need this line so we have some codes in ShoppingCartViewComponent
+        _logger.LogInformation("User logged out.");
+        if (returnUrl != null)
+            return LocalRedirect(returnUrl);
+        // This needs to be a redirect so that the browser performs a new
+        // request and the identity for the user gets updated.
+        return RedirectToPage();
     }
 }
