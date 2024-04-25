4cb00f3ae14a8d44eec6d9781169a06c1cea7875
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-04-25 14:34:57 +0700

    14 - 001 DBInitializer

---
+++ AleeVan.DataAccess/DbInitializer/DbInitializer.cs
@@ -0,0 +1,11 @@
+namespace AleeBook.DataAccess.DbInitializer;
+
+public class DbInitializer : IDbInitializer
+{
+    
+
+    public void Initializer()
+    {
+        throw new NotImplementedException();
+    }
+}
---
+++ AleeVan.DataAccess/DbInitializer/IDbInitializer.cs
@@ -0,0 +1,6 @@
+namespace AleeBook.DataAccess.DbInitializer;
+
+public interface IDbInitializer
+{
+    void Initializer();
+}
