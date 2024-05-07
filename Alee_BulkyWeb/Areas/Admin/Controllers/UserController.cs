8aff5aa5713cbc00a9febfc3d7274e6c84bafeea
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-05-07 14:32:28 +0700

    15 - 001 Display User List

---
+++ Alee_BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using AleeBook.DataAccess.Data;
+using AleeBook.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace AleeBookWeb.Areas.Admin.Controllers;
+
+[Area("Admin")]
+[Authorize(Roles = SD.Role_Admin)]
+public class UserController : Controller
+{
+    private readonly ApplicationDbContext _db;
+
+    public UserController(ApplicationDbContext db)
+    {
+        _db = db;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    #region API CALLS
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        var objUserList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+        return Json(new { data = objUserList });
+    }
+
+    [HttpDelete]
+    public IActionResult Delete(int? id)
+    {
+        return Json(new { success = true, message = "Delete Successful" });
+    }
+
+    #endregion API CALLS
+}
---
+++ Alee_BulkyWeb/wwwroot/js/user.js
@@ -0,0 +1,34 @@
+var dataTable;
+
+$(document).ready(function () {
+    loadDataTable();
+});
+
+// DataTables.net
+function loadDataTable() {
+    dataTable = $('#tblData').DataTable({
+        "ajax": {url: '/admin/user/getall'},
+        "columns": [
+            {data: 'name', "width": "15%"},
+            {data: 'email', "width": "15%"},
+            {data: 'phoneNumber', "width": "15%"},
+            {data: 'company.name', "width": "15%"},
+            {data: '', "width": "10%"},
+
+            {
+                data: 'id',
+                "render": function (data) {
+                    return `<div class="w-75 btn-group " role="group">
+                            <a href="/admin/company/Upsert?id=${data}" class="btn btn-primary mx-2">
+                                <i class="bi bi-pencil-square"></i>Edit
+                            </a>
+                            
+                        </div>`;
+                },
+                "width": "25%"
+            }
+
+        ]
+    });
+}
+
