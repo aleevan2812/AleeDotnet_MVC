1517676591c2668a45189e7645e45f8997865e0c
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-05-07 14:38:37 +0700

    15 - 002 Display Company Name

--- Alee_BulkyWeb/Areas/Admin/Controllers/UserController.cs
+++ Alee_BulkyWeb/Areas/Admin/Controllers/UserController.cs
@@ -28,6 +28,12 @@ public class UserController : Controller
     public IActionResult GetAll()
     {
         var objUserList = _db.ApplicationUsers.Include(u => u.Company).ToList();
+
+        foreach (var user in objUserList)
+        {
+            if (user.Company == null)
+                user.Company = new() { Name = "" };
+        }
         return Json(new { data = objUserList });
     }
 
