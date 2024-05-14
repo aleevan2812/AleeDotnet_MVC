097606fff195eab59100f0b3956238f15bfb5e0b
Alee Van <93045490+aleevan2812@users.noreply.github.com>
2024-05-14 23:16:29 +0700

    fix spelling err: "Guild" to "Guide"

--- Alee_BulkyWeb/Areas/Customer/Controllers/HomeController.cs
+++ Alee_BulkyWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,93 +1,93 @@
-using System.Diagnostics;
-using System.Security.Claims;
 using AleeBook.DataAccess.Repository.IRepository;
 using AleeBook.Models;
 using AleeBook.Utility;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
 
 namespace AleeBookWeb.Areas.Customer.Controllers;
 
 [Area("Customer")]
 public class HomeController : Controller
 {
-    private readonly ILogger<HomeController> _logger;
-    private readonly IUnitOfWork _unitOfWork;
+	private readonly ILogger<HomeController> _logger;
+	private readonly IUnitOfWork _unitOfWork;
 
-    public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
-    {
-        _logger = logger;
-        _unitOfWork = unitOfWork;
-    }
+	public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
+	{
+		_logger = logger;
+		_unitOfWork = unitOfWork;
+	}
 
-    public IActionResult Index()
-    {
-        var productList = _unitOfWork.Product.GetAll(includeProperties: "Category,ProductImages");
+	public IActionResult Index()
+	{
+		var productList = _unitOfWork.Product.GetAll(includeProperties: "Category,ProductImages");
 
-        return View(productList);
-    }
+		return View(productList);
+	}
 
-    public IActionResult Details(int productId)
-    {
-        var cart = new ShoppingCart
-        {
-            Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category,ProductImages"),
-            Count = 1,
-            ProductId = productId
-        };
+	public IActionResult Details(int productId)
+	{
+		var cart = new ShoppingCart
+		{
+			Product = _unitOfWork.Product.Get(u => u.Id == productId, includeProperties: "Category,ProductImages"),
+			Count = 1,
+			ProductId = productId
+		};
 
-        return View(cart);
-    }
+		return View(cart);
+	}
 
-    [HttpPost]
-    [Authorize]
-    public IActionResult Details(ShoppingCart shoppingCart)
-    {
-        var claimsIdentity = (ClaimsIdentity)User.Identity;
-        var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
-        // noi luu tru userId
-        shoppingCart.ApplicationUserId = userId;
+	[HttpPost]
+	[Authorize]
+	public IActionResult Details(ShoppingCart shoppingCart)
+	{
+		var claimsIdentity = (ClaimsIdentity)User.Identity;
+		var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+		// noi luu tru userId
+		shoppingCart.ApplicationUserId = userId;
 
-        var cartFromDb =
-            _unitOfWork.ShoppingCart.Get(u => u.ApplicationUserId == userId && u.ProductId == shoppingCart.ProductId);
+		var cartFromDb =
+			_unitOfWork.ShoppingCart.Get(u => u.ApplicationUserId == userId && u.ProductId == shoppingCart.ProductId);
 
-        if (cartFromDb != null)
-        {
-            // shopping cart exists
-            cartFromDb.Count += shoppingCart.Count;
-            _unitOfWork.ShoppingCart.Update(cartFromDb);
-            _unitOfWork.Save();
-        }
-        else
-        {
-            // add cart record
-            _unitOfWork.ShoppingCart.Add(shoppingCart);
-            _unitOfWork.Save();
+		if (cartFromDb != null)
+		{
+			// shopping cart exists
+			cartFromDb.Count += shoppingCart.Count;
+			_unitOfWork.ShoppingCart.Update(cartFromDb);
+			_unitOfWork.Save();
+		}
+		else
+		{
+			// add cart record
+			_unitOfWork.ShoppingCart.Add(shoppingCart);
+			_unitOfWork.Save();
 
-            HttpContext.Session.SetInt32(SD.SessionCart,
-                _unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId).Count());
-        }
+			HttpContext.Session.SetInt32(SD.SessionCart,
+				_unitOfWork.ShoppingCart.GetAll(u => u.ApplicationUserId == userId).Count());
+		}
 
-        //_unitOfWork.ShoppingCart.Add(shoppingCart);
-        TempData["success"] = "Cart updated successfully";
+		//_unitOfWork.ShoppingCart.Add(shoppingCart);
+		TempData["success"] = "Cart updated successfully";
 
-        //return View();
-        return RedirectToAction(nameof(Index)); // = "Index"
-    }
+		//return View();
+		return RedirectToAction(nameof(Index)); // = "Index"
+	}
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-    
-    public IActionResult Guild()
-    {
-        return View();
-    }
+	public IActionResult Privacy()
+	{
+		return View();
+	}
 
-    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
-    public IActionResult Error()
-    {
-        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
-    }
+	public IActionResult Guide()
+	{
+		return View();
+	}
+
+	[ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+	public IActionResult Error()
+	{
+		return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+	}
 }
